86e57baba2a2db7746c0dc6b2112f42f7a1b20d1
Daeki <kimdaeki@gmail.com>
2018-10-26 11:20:27 +0900

    Add Pager

---
+++ src/com/iu/page/RowNumber.java
@@ -0,0 +1,30 @@
+package com.iu.page;
+
+public class RowNumber {
+	
+	private int starRow;
+	private int lastRow;
+	private Search search;
+	
+	public int getStarRow() {
+		return starRow;
+	}
+	public void setStarRow(int starRow) {
+		this.starRow = starRow;
+	}
+	public int getLastRow() {
+		return lastRow;
+	}
+	public void setLastRow(int lastRow) {
+		this.lastRow = lastRow;
+	}
+	public Search getSearch() {
+		return search;
+	}
+	public void setSearch(Search search) {
+		this.search = search;
+	}
+	
+	
+
+}
